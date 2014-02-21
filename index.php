e77b58478a118885b549620743b2069a7d51892a
Saravanan <saravanan@visolve.com>
2014-02-21 15:48:15 +0530

    Second try

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
-// Page créé par Shepard [Fabian Pijcke] <Shepard8@laposte.net>
-// Arno Esterhuizen <arno.esterhuizen@gmail.com>
-// et Romain Bourdon <rromain@romainbourdon.com>
-// et Hervé Leclerc <herve.leclerc@alterway.fr>
-//  
-// Mise a jour par Herve Leclerc herve.leclerc@alterway.fr
-// Ici par Mark James <http://www.famfamfam.com/lab/icons/silk/>
-
 
 
 //chemin jusqu'au fichier de conf de WampServer
