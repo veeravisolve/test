bdb6ab1f795d005f1e87014f5d6efcf5c910f52a
veera.R <veera@visolve.com>
2014-02-21 14:31:26 +0530

    lorum addded info.php - part5

--- info.php
+++ info.php
@@ -10,7 +10,8 @@ this is test
 			
 			
 			
-			
+						
+Nam iaculis nisl nec sapien tristique sagittis. Duis nec aliquam nunc, sit amet mattis lorem. Curabitur sed est malesuada, malesuada elit a, sagittis enim. Nullam in nulla vel est sodales suscipit. Mauris euismod dui magna, in egestas ipsum commodo at. Nunc eget lorem eros. Etiam tincidunt eros libero. 
 			
 			<br />
 			<div class="row">
