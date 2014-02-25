94b8c7b44a8cb673b7d36ce0422651efe591c526
veera.R <veera@visolve.com>
2014-02-25 11:52:44 +0530

    Lorum file updated

--- lorum.php
+++ lorum.php
@@ -3,3 +3,7 @@
 Nulla hendrerit sapien at massa viverra, vitae cursus mi convallis. Mauris a dignissim ligula, sed aliquam odio. Curabitur vel cursus est, id placerat augue. Mauris non magna diam. Suspendisse accumsan commodo erat id iaculis. Morbi laoreet sagittis ligula, nec rhoncus enim scelerisque sed. Nulla eu velit sit amet purus convallis rhoncus ultrices interdum sapien. Nullam leo metus, imperdiet id placerat sed, gravida quis velit. Proin accumsan dui mauris. Etiam scelerisque arcu massa, sit amet placerat turpis volutpat at. Proin ut malesuada odio. Praesent non commodo ante, vel condimentum nulla. Cras iaculis tellus eget turpis varius dictum.
 
 Sed lobortis malesuada urna nec euismod. In a lorem quis magna vehicula tempor. Curabitur fringilla mauris eget luctus adipiscing. Nam aliquet urna vitae auctor molestie. Fusce facilisis, urna placerat suscipit elementum, risus arcu dapibus arcu, a feugiat sem ante a arcu. Ut mollis lacinia mauris, sit amet pharetra mauris pretium ac. Phasellus vel sagittis orci. In venenatis eleifend egestas. Nam pretium porta ipsum sed malesuada. Ut leo augue, placerat vitae adipiscing vel, vehicula volutpat libero. In purus mi, rhoncus sit amet tellus sit amet, gravida scelerisque nisl. Quisque dictum consectetur odio ut congue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer libero ipsum, imperdiet ut mi sit amet, pharetra egestas tortor. 
+
+
+
+vvvv text change J   CCCC 
